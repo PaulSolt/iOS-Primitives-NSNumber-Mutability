4b3e8b0a399c175e0d976bac4a90b3b49d5968fe
Paul Solt <PaulSolt@gmail.com>
2019-11-07 13:53:27 -0500

    Updated code to show solution to challenge

--- iOS9-Pointers/iOS9-Pointers/main.m
+++ iOS9-Pointers/iOS9-Pointers/main.m
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
+// Function declaration
+void cStringExercise(void);
+
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 
@@ -46,6 +49,46 @@ int main(int argc, const char * argv[]) {
 		*addressOfValue = 75;
 		printf("CHANGED value: %li\n", value);
 
+		char *str = "Hello World!";
+		printf("str: %p\n", str); // %p = print pointer address (base 16 = hexidecimal)
+		printf("str: %s\n", str); // %s = print as a c-string
+
+		char letter5 = str[4];
+		printf("letter5: %c\n", letter5);	// %c = print a character   \n = newline
+		
+		// Pointer math (pointer arithmetic)
+		letter5 = *(str + 6);
+		printf("letter5: %c\n", letter5);
+		
+		
+		cStringExercise();
+		
 	}
 	return 0;
+}
+
+
+void cStringExercise(void) {
+    char *message = "Hello xxxxxxxxxxxxxxxxxxxx!"; // '\0' = null terminated
+
+    printf("message pointer: %p\n", message);
+    printf("message: %s\n", message);
+
+    unsigned long length = strlen(message);
+	printf("Please allocate a string of size: %zu\n", sizeof(char) * length + 1);
+    char *ptr = malloc(sizeof(char) * length + 1);	// memory allocation
+    ptr = strcpy(ptr, message);
+
+	// Set your name!
+
+    ptr[6] = 'P'; // START HERE
+	ptr[7] = 'a';
+	ptr[8] = 'u';
+	ptr[9] = 'l';
+	ptr[10] = '\0';
+	printf("message: %s\n", ptr);
+
+	// Expected: message: Hello Paul Solt!    // No x's!
+	
+	free(ptr);
 }
