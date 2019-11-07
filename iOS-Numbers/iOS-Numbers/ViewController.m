33388515b27917e8dc7223a0ec3bd231338cf529
Paul Solt <PaulSolt@gmail.com>
2019-11-07 14:02:37 -0500

    Added objects vs. primitives

--- iOS-Numbers/iOS-Numbers/ViewController.m
+++ iOS-Numbers/iOS-Numbers/ViewController.m
@@ -16,7 +16,28 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+	// Objects vs. Primitives
+
+	// NSString = Object in Objc
+	// NSNumber
+	// UITableViewCell
+	// UIImage
+	// .mov
+
+	// int != NSObject (id == object)
+
+	//	NSArray *numArray = [NSArray arrayWithObjects:3, 4, 5, nil];
+
+	NSNumber *one = @1; // [NSNumber numberWithInt:1];
+	NSNumber *two = @2; // [NSNumber numberWithInt:2];
+	NSNumber *three = @3; // [NSNumber numberWithInt:3];
+
+	//	NSArray *numArray = [NSArray arrayWithObjects:one, two, three, nil];
+	NSArray *numArray = @[@1, @2, @3, @3.14, @YES];
+
+	printf("numArray: %s", numArray.description.UTF8String);
+
 }
 
 
