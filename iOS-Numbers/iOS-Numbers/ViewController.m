7a5ba4b858cbaef92515fb48e6d8fd6ce51d0b3d
Paul Solt <PaulSolt@gmail.com>
2019-11-07 21:32:26 -0500

    Commented out unused code from demo

--- iOS-Numbers/iOS-Numbers/ViewController.m
+++ iOS-Numbers/iOS-Numbers/ViewController.m
@@ -30,9 +30,9 @@
 
 	//	NSArray *numArray = [NSArray arrayWithObjects:3, 4, 5, nil];
 
-	NSNumber *one = @1; // [NSNumber numberWithInt:1];
-	NSNumber *two = @2; // [NSNumber numberWithInt:2];
-	NSNumber *three = @3; // [NSNumber numberWithInt:3];
+//	NSNumber *one = @1; // [NSNumber numberWithInt:1];
+//	NSNumber *two = @2; // [NSNumber numberWithInt:2];
+//	NSNumber *three = @3; // [NSNumber numberWithInt:3];
 
 	//	NSArray *numArray = [NSArray arrayWithObjects:one, two, three, nil];
 	NSArray *numArray = @[@1, @2, @3, @3.14, @YES];
